ba937a32e72d861414b57baf67120debc7a8c323
Artur <magisterbrownie@gmail.com>
2024-07-28 23:03:46 +0200

    State passing

--- nob.c
+++ nob.c
@@ -10,43 +10,46 @@
 
 #define MAT_SIDE 10
 
-typedef void visulizer();
+typedef void visualizer(void *state);
+typedef void *starter();
 
+visualizer *matdraw;
+starter *matinit;
 
 void *vishandle;
 char *errmsg;
-visulizer *hot_redrawer()
+void *hot_redrawer()
 {
     Nob_Cmd build = {0};
     char *path = "artifacts/vis.so";
-    nob_cmd_append(&build, "gcc", "visualizer.c", "-o", path, "-shared");
+    nob_cmd_append(&build, "gcc", "generator.c", "visualizer.c", "-o", path, "-shared");
     if(!nob_cmd_run_sync(build))
     {
-        errmsg = "Build of viz failed";
+        errmsg = "Build failed";
         return NULL;
     }
     if(vishandle != NULL)
         dlclose(vishandle);
     vishandle = dlopen(path, RTLD_NOW);
     errmsg = NULL;
-    return dlsym(vishandle, "main");
+    matdraw = dlsym(vishandle, "draw");
+    matinit = dlsym(vishandle, "init");
+    return matinit();
 }
 
 int main(int argc, char **argv) 
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
     errmsg = NULL;
-    visulizer *matdraw = hot_redrawer();
-    printf("ad");
+    void *state = hot_redrawer();
     InitWindow(800, 500, "main window");
-    printf("Heyy\n");
     while(!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
         if(IsKeyPressed(KEY_R))
-            matdraw = hot_redrawer();
+            state = hot_redrawer();
         if(errmsg == NULL) 
-            matdraw();
+            matdraw(state);
         else
             DrawText(errmsg, 0, 0, 20, RED);
         EndDrawing();
--- visualizer.c
+++ visualizer.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "raylib/raylib.h"
-
-//(Color){255, 0, 0, 255}
-//(Color){255, 0, 0, 0}
-#define MAT_SIDE 10
-
-void rand_init(size_t side ,float matrix[side][side])
-{
-    for(int x=0;x<side;x++)
-        for(int y=0;y<side;y++)
-            matrix[x][y] = (float)rand()/(float)RAND_MAX;
-}
+#include "state.h"
 
 void draw_mat(size_t side ,float matrix[side][side], Vector2 pos, Color col)
 {
@@ -20,12 +10,8 @@ void draw_mat(size_t side ,float matrix[side][side], Vector2 pos, Color col)
             DrawRectangle(pos.x+x*30, pos.y+y*30, 30, 30, ColorAlpha(col, matrix[x][y]));
 }
 
-void main()
+void draw(struct state *data)
 {
-    float mata[MAT_SIDE][MAT_SIDE];
-    float matb[MAT_SIDE][MAT_SIDE];
-    rand_init(MAT_SIDE, mata);
-    rand_init(MAT_SIDE, matb);
-    draw_mat(MAT_SIDE, mata, (Vector2){30, 0}, BLUE);
-    draw_mat(MAT_SIDE, matb, (Vector2){430, 0}, RED);
+    draw_mat(MAT_SIDE, data->mata, (Vector2){30, 30}, BLUE);
+    draw_mat(MAT_SIDE, data->matb, (Vector2){430, 30}, RED);
 }
