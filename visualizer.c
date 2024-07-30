b9a15eb3f91037aa9476de1c9f4ea3d7aeca0a27
Artur <magisterbrownie@gmail.com>
2024-07-30 13:51:42 +0200

    Mat mulll

--- visualizer.c
+++ visualizer.c
@@ -3,15 +3,26 @@
 #include "raylib/raylib.h"
 #include "state.h"
 
-void draw_mat(size_t side ,float matrix[side][side], Vector2 pos, Color col)
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+void draw_mat(size_t a, size_t b, int matrix[a][b], Vector2 pos, Color col, int cellsz)
 {
-    for(int x=0;x<side;x++)
-        for(int y=0;y<side;y++)
-            DrawRectangle(pos.x+x*30, pos.y+y*30, 30, 30, ColorAlpha(col, matrix[x][y]));
+    int mv = 0;
+    for(int x=0;x<a;x++)
+        for(int y=0;y<b;y++)
+            mv = MAX(matrix[x][y], mv);
+
+    for(int x=0;x<a;x++)
+        for(int y=0;y<b;y++)
+        {
+            DrawRectangle(pos.y+y*cellsz, pos.x+x*cellsz, cellsz, cellsz, ColorAlpha(col, (float)matrix[x][y]/mv));
+            DrawText(TextFormat("%d", matrix[x][y]), pos.y+y*cellsz+8, pos.x+x*cellsz+2, cellsz, BLACK);
+        }
 }
 
 void draw(struct state *data)
 {
-    draw_mat(MAT_SIDE, data->mata, (Vector2){30, 30}, BLUE);
-    draw_mat(MAT_SIDE, data->matb, (Vector2){430, 30}, RED);
+    draw_mat(SIDA, COMMON, data->mata, (Vector2){25, 25}, BLUE, 25);
+    draw_mat(COMMON, SIDB, data->matb, (Vector2){25, 25*(COMMON+2)}, RED, 25);
+    draw_mat(SIDA, SIDB, data->matc, (Vector2){25, 25*(SIDB+COMMON+3)}, GREEN, 25);
 }
