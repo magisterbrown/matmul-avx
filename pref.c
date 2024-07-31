251ac2a9af7df68330da03173d282699b7c9cd8d
Artur <magisterbrownie@gmail.com>
2024-07-31 15:45:52 +0200

    only perf

--- pref.c
+++ pref.c
@@ -24,46 +24,25 @@ void measurement()
 
 int main()
 {
-    sem_t *sem;
-    pid_t pid = fork();
-    switch(pid)
-    {
-    case -1:
-        return 1;
-    case 0:
-        sem = sem_open(SNAME, 0);
-        sem_wait(sem);
+    struct perf_event_attr pe = {0};
 
-        printf("YayKj:w\n");
-        measurement();
+    pe.type = PERF_TYPE_HARDWARE;
+    pe.size = sizeof(struct perf_event_attr);
+    pe.config = PERF_COUNT_HW_INSTRUCTIONS;
+    pe.exclude_kernel = 1;
 
-        sem_close(sem);
-        exit(0);
-    default:
-        sem = sem_open(SNAME, O_CREAT, 0644, 0);
-        struct perf_event_attr pe = {0};
+    int fd = syscall(__NR_perf_event_open, &pe, 0, -1, -1, 0);
+    ioctl(fd, PERF_EVENT_IOC_RESET, 0);
+    ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
 
-        pe.type = PERF_TYPE_HARDWARE;
-        pe.size = sizeof(struct perf_event_attr);
-        pe.config = PERF_COUNT_HW_INSTRUCTIONS;
-        pe.exclude_kernel = 1;
+    measurement();
 
-        int fd = syscall(__NR_perf_event_open, &pe, 0, -1, -1, 0);
-        ioctl(fd, PERF_EVENT_IOC_RESET, 0);
-        ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
-        printf("Measuring instruction count for this printf\n");
-
-        ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
-        long int count;
-        read(fd, &count, sizeof(long long));
-        
-        printf("Used %lld instructions\n", count);
-        
-        close(fd);
-        printf("FD: %d\n", fd);
-        sem_post(sem);
-        printf("Par\n");
-        sem_close(sem);
-    }
+    ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
+    long int count;
+    read(fd, &count, sizeof(long long));
+    
+    printf("Used %lld instructions\n", count);
+    
+    close(fd);
     return 0;
 }
